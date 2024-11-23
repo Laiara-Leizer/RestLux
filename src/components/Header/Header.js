292285ef55b00ecf7097d3e9675abbcbc225a9cf
Laiara-Leizer <laiaraleizer06@gmail.com>
2024-11-23 18:54:50 -0300

    Funcionando, com logo, com favicon, com rotas e responsivo 100% sem media

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import Logo from "./img/RestLux.png"
 // import ThemeToggle from '../ThemeToggle/';
 
 function Header() {
   return (
     <header className="header">
       <nav>
+
+      <img className='Logo' src={Logo }/>
         <ul>
         {/* <ThemeToggle /> */}
+          
           <li><Link to="/">Home</Link></li>
           <li><Link to="/restaurants">Restaurantes</Link></li>
           <li><Link to="/foods">Comidas</Link></li>
